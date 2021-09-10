19da5168744a8be6d76d58d3cd1ff68897508d12
vraghuvaran <vraghuvaran123@gmail.com>
2021-09-10 18:26:28 +0000

    added code

--- PaymentFrontend/src/app/dashboard/viewprofile/viewprofile.component.ts
+++ PaymentFrontend/src/app/dashboard/viewprofile/viewprofile.component.ts
@@ -1,5 +1,8 @@
+import { NULL_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { Bank } from 'src/app/models/bank';
 import { Customer } from 'src/app/models/customer';
+import { CustomerService } from 'src/app/services/customer.service';
 
 @Component({
   selector: 'app-viewprofile',
@@ -8,12 +11,22 @@ import { Customer } from 'src/app/models/customer';
 })
 export class ViewprofileComponent implements OnInit {
 
-  profile?: Customer
-  constructor() { }
+  bank?: Bank;
+  profile?: Customer;
+  constructor(private customerservice: CustomerService) { }
 
   ngOnInit(): void {
 
-    
+    this.customerservice.getCustomer().subscribe((data: any)=>{
+
+      this.profile = data.customer;
+      
+
+    },(errror)=>{
+
+
+
+    })
 
   }
 
